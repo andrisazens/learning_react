e0fb0648748db8f2e49bcb506ad339deb7e8d3c0
Andris Azens <andris.azens@inbox.lv>
2016-05-12 22:35:00 +0300

    Comment Box hello

---
+++ tutorial/public2/scripts/example.js
@@ -0,0 +1,11 @@
+
+var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div class="commentBox">Hello, world! I am a CommentBox.</div>
+        );
+    }
+});
+ReactDOM.render(
+    <CommentBox/>, document.getElementById('content')
+);
