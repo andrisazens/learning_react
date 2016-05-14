3fd8c4734ce88eb8f6db1dc06cf4108d229da845
Andris Azens <andris.azens@inbox.lv>
2016-05-14 21:50:04 +0300

    Comment list display

--- tutorial/public2/scripts/example.js
+++ tutorial/public2/scripts/example.js
@@ -1,9 +1,22 @@
 
+var Comment = React.createClass({
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    { this.props.author }
+                </h2>                
+                { this.props.children }
+            </div>
+        );
+    }
+});
 var CommentList = React.createClass({
     render: function () {
         return (
-            <div class="commentList">
-                Hello, world! I am a CommentList
+            <div className="commentList">
+                <Comment author="Pete Hunt">This is one comment</Comment>
+                <Comment author="Jordan Walke">This is *another* comment</Comment>
             </div>
         );
     }
@@ -11,7 +24,7 @@ var CommentList = React.createClass({
 var CommentForm = React.createClass({
     render: function () {
         return (
-            <div class="commentForm">
+            <div className="commentForm">
                 Hello, world! I am a CommentForm
             </div>
         );
@@ -21,7 +34,7 @@ var CommentForm = React.createClass({
 var CommentBox = React.createClass({
     render: function () {
         return (
-            <div class="commentBox">
+            <div className="commentBox">
                 <h1>Comments</h1>
                 <CommentList/>
                 <CommentForm/>
