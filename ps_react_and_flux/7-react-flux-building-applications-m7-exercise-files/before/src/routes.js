aff5611d49b691809e8145e3c30e4fb4f6775daf
Andris Azens <andris.azens@inbox.lv>
2016-05-25 22:54:10 +0300

    Demo 7; Redirects

--- ps_react_and_flux/7-react-flux-building-applications-m7-exercise-files/before/src/routes.js
+++ ps_react_and_flux/7-react-flux-building-applications-m7-exercise-files/before/src/routes.js
@@ -5,6 +5,7 @@ var Router = require("react-router");
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
+var RedirectRoute = Router.Redirect;
 
 var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
@@ -12,6 +13,9 @@ var routes = (
         <Route name="authors" handler={require('./components/authors/authorPage')} />
         <Route name="about" handler={require('./components/about/aboutPage')} />
         <NotFoundRoute handler={require('./components/notFoundPage')} />
+        <RedirectRoute from="about-us" to="about" />
+        <RedirectRoute from="awthurs" to="authors" />
+        <RedirectRoute from="about/*" to="about" />
     </Route>
 );
 
