910dd6648a3beff2a053df0ebf737a286e665392
Andris Azens <andris.azens@inbox.lv>
2016-05-28 18:19:37 +0300

    Demo 7: React binding not working, silent

--- ps_react_and_flux/8-react-flux-building-applications-m8-exercise-files/before/src/components/authors/authorForm.js
+++ ps_react_and_flux/8-react-flux-building-applications-m8-exercise-files/before/src/components/authors/authorForm.js
@@ -8,10 +8,12 @@ var AuthorForm = React.createClass({
             <form>
                 <h1>Manage Author</h1>
                 <label htmlFor="firstName">First Name</label>
-                <input type="text" className="form-control" placeholder="First Name" ref="firstName" value="" />
+                <input type="text" className="form-control" placeholder="First Name" 
+                    ref="firstName" value={this.props.author.firstName} onChange={this.props.onChange} />
                 <br />
                 <label htmlFor="firstName">Last Name</label>
-                <input type="text" className="form-control" placeholder="Last Name" ref="lasttName" value="" />
+                <input type="text" className="form-control" placeholder="Last Name" 
+                    ref="lasttName" value={this.props.author.lastName} onChange={this.props.onChange} />
                 <br />
                 <input type="submit" value="Save" className="btn btn-default" />
             </form>
