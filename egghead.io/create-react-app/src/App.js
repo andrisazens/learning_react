5c47dd7df207bbcceeb0f88a910809e05c0db1f9
Andris Azens <andris.azens@inbox.lv>
2017-04-02 15:30:21 +0300

    Custom prop validation

--- egghead.io/create-react-app/src/App.js
+++ egghead.io/create-react-app/src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
           <h2>Welcome to React! txt: { this.props.txt }, category: {this.props.cat}, state text: {this.state.txt}, state cat: {this.state.cat} </h2>
           <Widget update={this.update.bind(this)} />
           <Button>I <Heart></Heart> React</Button>
+          <Title text="this text large"></Title>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -30,12 +31,25 @@ class App extends Component {
   }
 }
 
-const Widget = (props) => <input type="text" onChange={props.update} />
 const Button = (props) => <button>{props.children}</button>
+const Title = (props) => <h1>Title: {props.text}</h1>
+const Widget = (props) => <input type="text" onChange={props.update}/>
 
 class Heart extends Component {
   render(){
     return <span>&hearts;</span>
+  }
+}
+
+Title.propTypes = {
+  //text: React.PropTypes.string.isRequired
+  text(props, propName, component) {
+    if (!(propName in props)){
+      return new Error(`missing ${propName}`);
+    }    
+    if (props[propName].length < 10){
+      return new Error(`${propName} too short`);
+    }
   }
 }
 
