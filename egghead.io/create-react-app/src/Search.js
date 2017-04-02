a61ef5c54e688ab5dd71e838e6558e1189aab1f4
Andris Azens <andris.azens@inbox.lv>
2017-04-02 17:39:38 +0300

    Search done

--- egghead.io/create-react-app/src/Search.js
+++ egghead.io/create-react-app/src/Search.js
@@ -10,11 +10,17 @@ class App extends Component {
             .then(response => response.json())
             .then(({results: items}) => this.setState({ items }))
     }
+    filter(e) {
+        this.setState({ filter: e.target.value })
+    }
     render() {
         let items = this.state.items;
-        
+        if (this.state.filter) {
+            items = items.filter(item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+        }
         return (
             <div>
+                <input type="text" onChange={this.filter.bind(this)} />
                 { items.map(item => <Person key={item.name} person={item} />) }
             </div>
         )
